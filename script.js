4e695898fea9bdf2c83bd2a981560329950db029
mcharland555 <75118322+mcharland555@users.noreply.github.com>
2022-02-01 23:30:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 function compute()
 {
-    var resultString = "";
-    var principal = document.getElementById("principal").value;
+    // Calculate interest
 
+    // Get form values
+    var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-
-    var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
+    // Calculation
+    var interest = principal * years * rate /100;
+    
+    // Concatenate result
     const resultString =  `If you deposit principal <mark>${principal}</mark>,<br />` +
                         `at an interest rate of <mark>${rate}%</mark>.<br />` +
                         `You will receive an amount of <mark>${interest}</mark>,<br />` + 
                         `in the year <mark>${year}</mark>`;
 
+    // Write result
     document.getElementById("result").innerHTML=resultString;
 }
 
 function updateRate() 
 {
+    // Update shown rate value
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=`${rateval}%`;
 }
 
-function validateForm() {
-  let amount = document.forms["calculatorForm"]["amount"].value;
-  if (amount == "" || amount <= 0) {
-    alert("Enter a positive number");
-    return false;
-  }
-  compute()
+function checkdata() {
+    // Check if amount is a positive number
+    let value = document.forms["calculatorForm"]["principal"].value;
+    if (value == "" || value <= 0) {
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    else {
+        compute();
+    }  
 }
         
