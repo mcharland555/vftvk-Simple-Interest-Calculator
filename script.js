3b97c34aab6b6165a7fb794f7e97ba2851a5b23e
mcharland555 <75118322+mcharland555@users.noreply.github.com>
2022-02-01 21:11:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var resultString = "";
+    var principal = document.getElementById("principal").value;
+
+    if (principal <= 0 || principal == null){
+        alert("Enter a positive number");
+    }
+
+    else{
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+
+        resultString =  `If you deposit principal ${principal},\n` +
+                        `at an interest rate of ${rate}%.\n` +
+                        `You will receive an amount of ${interest},\n` + 
+                        `in the year ${year}`;
+    }
     
+
+    document.getElementById("result").innerText=resultString;
+
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+     
