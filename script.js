ae5227ac6b2b00923f12634af78a287da7725793
mcharland555 <75118322+mcharland555@users.noreply.github.com>
2022-02-02 00:12:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function compute()
     // Calculate interest
 
     // Get form values
-    var principal = document.getElementById("principal").value.replace(/^0+/, '');
+    var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var year = new Date().getFullYear()+parseInt(years);
@@ -12,7 +12,7 @@ function compute()
     var interest = principal * years * rate /100;
     
     // Concatenate result
-    const resultString =  `If you deposit <mark>${principal}</mark>,<br />` +
+    const resultString =  `If you deposit <mark>${principal.replace(/^0+/, '')}</mark>,<br />` +
                         `at an interest rate of <mark>${rate}%</mark>.<br />` +
                         `You will receive an amount of <mark>${interest}</mark>,<br />` + 
                         `in the year <mark>${year}</mark>`;
